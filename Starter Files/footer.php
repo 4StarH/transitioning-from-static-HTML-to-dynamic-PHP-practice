16d65e1b9f6ef91d800d0933b66121dd1b0fe61a
Forrester Hinds <37647146+4StarH@users.noreply.github.com>
2019-03-26 15:57:35 -0400

    Add files via upload

---
+++ Starter Files/footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div>
+            <footer>
+                <p>&copy; 2018 CST8285. All Rights Reserved.</p>
+            </footer>
+        </div><!-- End Wrapper -->
+    </body>
+</html>
